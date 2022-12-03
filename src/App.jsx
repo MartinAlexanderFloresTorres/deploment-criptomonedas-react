6a3ca71bd144c03f125efa7f40a56c0596a674a4
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-12-03 16:13:36 -0500

    add files logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
-import styled from "@emotion/styled";
-import Formulario from "./components/Formulario";
-import Resultado from "./components/Resultado";
-import Spiner from "./components/Spiner";
-import imgCripto from "../src/img/imagen-criptos.png";
+import { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spiner from './components/Spiner'
+import imgCripto from '../src/img/imagen-criptos.png'
+import Footer from './components/Footer'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -21,24 +22,24 @@ const Contenedor = styled.div`
   @media (max-width: 280px) {
     padding: 20px 10px;
   }
-`;
+`
 
 const Imagen = styled.img`
   display: block;
   max-width: 400px;
   width: 100%;
   margin: 0 auto;
-`;
+`
 
 const Heading = styled.h1`
-  font-family: "Lato", sans-serif;
+  font-family: 'Lato', sans-serif;
   color: #fff;
   text-align: center;
   font-weight: 700;
   font-size: 34px;
   text-transform: capitalize;
   &::after {
-    content: "";
+    content: '';
     width: 100px;
     height: 6px;
     background: #66a2fe;
@@ -51,44 +52,46 @@ const Heading = styled.h1`
   @media (max-width: 392px) {
     font-size: 28px;
   }
-`;
+`
 
 function App() {
-  const [monedas, setMonedas] = useState({});
-  const [resultado, setResultado] = useState({});
-  const [cargando, setCargando] = useState(false);
+  const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   //=============== mostrar cotizacion ===============//
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
-        setResultado({});
-        setCargando(true);
-        const { moneda, criptoMoneda } = monedas;
-        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`;
-        const respuesta = await fetch(url);
-        const resultados = await respuesta.json();
-        const datos = resultados.DISPLAY[criptoMoneda][moneda];
-        setResultado(datos);
-        setCargando(false);
-      };
-      cotizarCripto();
+        setResultado({})
+        setCargando(true)
+        const { moneda, criptoMoneda } = monedas
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`
+        const respuesta = await fetch(url)
+        const resultados = await respuesta.json()
+        const datos = resultados.DISPLAY[criptoMoneda][moneda]
+        setResultado(datos)
+        setCargando(false)
+      }
+      cotizarCripto()
     }
-  }, [monedas]);
+  }, [monedas])
 
   return (
-    <Contenedor>
-      <Imagen src={imgCripto} alt="imagen" />
-      <div>
-        <Heading>Cotiza tus criptomonedas al instante</Heading>
-        <Formulario setMonedas={setMonedas} />
-        {cargando && <Spiner />}
-        {Object.keys(resultado).length > 0 && (
-          <Resultado resultado={resultado} />
-        )}
-      </div>
-    </Contenedor>
-  );
+    <>
+      <Contenedor>
+        <Imagen src={imgCripto} alt='imagen' />
+        <div>
+          <Heading>Cotiza tus criptomonedas al instante</Heading>
+          <Formulario setMonedas={setMonedas} />
+          {cargando && <Spiner />}
+          {Object.keys(resultado).length > 0 && <Resultado resultado={resultado} />}
+        </div>
+      </Contenedor>
+
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
